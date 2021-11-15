50572184b1968e39b769e1bab6448251dda96098
Your Name <mahtab.salahshoor@gmail.com>
2021-11-14 22:05:58 -0800

    updating my work on javascript codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 
-var generateBtn = document.querySelector("#generate");
+  var generateBtn = document.querySelector("#generate");
       var upperCase=["A", "B", "C", "D", "E", "F", 
       "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
       "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h",
@@ -16,58 +16,53 @@ var generateBtn = document.querySelector("#generate");
 
     var password = "";
     var empty="";
-    var userinput= "";
-
-// Add event listener to generate butto
-
-function writePassword() {
+    var userinput= "";``
+// Add event listener to generate button
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  return
   passwordText.value = password;
-  return ("")
-}
-generateBtn.addEventListener("click", writePassword);
-console.log(writePassword);
+  };
+  // event listener for genrate button
+  generateBtn.addEventListener("click", writePassword);
+  console.log(writePassword);
+  function generatePassword(){
+    var passwordLength= prompt( "How many characters would you like your password to contain?" );
 
-function generatePassword(){
-  var passwordLength= prompt( "How many characters would you like your password to contain?" );
+    if (passwordLength <= 8 && passwordLength >= 128) {
+      alert( "Please use characters between 8-24." );
+      console.log(passwordLength);  
+    }
+    else {
+      alert( "Please use characters between 8-24." );
+      var confirmSpecialCharacters = confirm( "Click OK to include special characters." );
+      var confirmNumbers = confirm( "Click OK to include numeric characters." );
+      var confirmLowerCase = confirm( "Click OK to include lowercase characters." );
+      var confirmUpperCase = confirm( "Click OK to include uppercase characters." );
 
-  if (passwordLength >= 8 && passwordLength > 128) {
-    console.log("passwordLength");
-    
- }
- 
-  else {
-    alert( "Please use characters between 8-24." );
-    var confirmSpecialCharacters = confirm( "Click OK to include special characters." );
-    var confirmNumbers = confirm( "Click OK to include numeric characters." );
-    var confirmLowerCase = confirm( "Click OK to include lowercase characters." );
-    var confirmUpperCase = confirm( "Click OK to include uppercase characters." );
-
-    console.log(confirmSpecialCharacters);
-    console.log(confirmNumbers);
-    console.log(confirmLowerCase);
-    console.log(confirmUpperCase);
-   }
+      console.log(confirmSpecialCharacters);
+      console.log(confirmNumbers);
+      console.log(confirmLowerCase);
+      console.log(confirmUpperCase);
+    }
     var passwordLength= prompt('please type your passwrod below then press ok to submit.')
     
-  if (passwordLength >= 8 && passwordLength > 128) {
-    console.log(passwordLength);
-    
- }
- 
-  else {
-    alert( "Please use characters between 8-24." );
-    console.log(alert);
+    if(confirmLowerCase === false && confirmUpperCase === false 
+      && confirmNumbers === false && confirmSpecialCase === false)  {
+      alert( "Please try again write your password." );
+      console.log(passwordLength); 
+    }
+    else {
+      alert('great job!');
+      console.log('greatjob');
    };
-
-
-   generateBtn.addEventListener("click", generatePassword);  
- if (confirmSpecialCharacters=== true){
+  generateBtn.addEventListener("click", generatePassword);  
+    if(confirmSpecialCharacters=== true){
       specialCharacters= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
-   "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
-   console.log(confirmSpecialCharacters);
- }
+    "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+    console.log(confirmSpecialCharacters);
+  }
    else {
       specialCharacters= empty;
    }
@@ -100,13 +95,11 @@ function generatePassword(){
    else {
        upperCase=empty;
    }
-  
-
-     for (let i = 0; i < length; i++) {
-      password += allowedCharacters[Math.floor(Math.random() * allowedCharacters.length)]
-    }
-    return password;
-  };
-   
-
+  }
+  var passwordFinal = "";
+  for (var i = 0; i < confirm.Length; i++) {
+    passwordFinal = passwordFinal + passwordArray[Math.floor(Math.random() * passwordArray.length)];
+  }
+  generateBtn.addEventListener("click", writePassword)
+  document.getElementById("placeholder");
   
